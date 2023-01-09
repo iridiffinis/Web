1066229c821e0138c271262c83c240d19facaebd
iridiffinis <102820231+iridiffinis@users.noreply.github.com>
2023-01-09 08:45:07 +0200

    Add files via upload

---
+++ tema 4/main/src/components/RobotList.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import RobotStore from '../stores/RobotStore'
+import Robot from './Robot'
+import RobotForm from './RobotForm'
+
+
+
+class RobotList extends Component {
+	constructor(){
+		super()
+		this.state = {
+			robots : []
+		}
+		this.store = new RobotStore()
+	}
+	componentDidMount(){
+		
+		this.setState({
+			robots : this.store.getRobots()
+		})
+		this.store.emitter.addListener('UPDATE', () => {
+			this.setState({
+				robots : this.store.getRobots()
+			})			
+		})
+
+		this.handleAdd = (robot)=>{
+			this.store.addRobot({
+				name : robot.name,
+				type: robot.type,
+				mass: robot.mass
+			});
+		}
+	}
+	render() {
+		return (
+			<div>
+				 <RobotForm onAdd={this.handleAdd}/>
+				{
+					this.state.robots.map((e, i) => 
+						<Robot item={e} key={i} />
+					)
+				}
+			</div>
+		)
+	}
+}
+
+export default RobotList
